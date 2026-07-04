18ec89e750b33c0814ad4ba840b5ab6cb6082f12
wassimabdelhedi <wassim.abdelhedi@enis.tn>
2026-07-04 13:06:47 +0100

    User service

--- docker_mini_projet/microservices-project/users-service/server.js
+++ docker_mini_projet/microservices-project/users-service/server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API is running inside Docker 🚀");
+  res.send("Users Service is running inside Docker 🚀");
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Users Service running on port 3000");
 });
