c63e220201405d325d0b1eb54e7aa20c950fbc25
ahmedmnifcreator <ahmed.mnif@enis.tn>
2026-07-06 21:34:32 +0100

    pipline agents

--- docker_mini_projet/microservices/users-service/server.js
+++ docker_mini_projet/microservices/users-service/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Users Service is running inside Docker 🚀");
+  res.send("Users Service is running inside Docker ggggg🚀");
 });
 
 app.listen(3000, () => {
